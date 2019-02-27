f03433082391283e99c9ad55d11b3e0f27ba0c0e
robbyhecht <robbyhecht@gmail.com>
2019-02-27 15:56:44 -0600

    going to merge with payment type master. orders edit in progress

--- src/components/ecommerce/orders/OrdersEdit.js
+++ src/components/ecommerce/orders/OrdersEdit.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Form } from 'semantic-ui-react'
 import { Link } from "react-router-dom";
 
 export default class OrdersEdit extends Component {
@@ -51,6 +51,60 @@ export default class OrdersEdit extends Component {
                         <label htmlFor="orderCustomer">Change Customer</label>
                         <input type="text" required className="form-control" onChange={this.handleFieldChange} id="customer" value={this.state.customer} />
                     </div>
+
+            <React.Fragment>
+            <Form className="OrdersEditForm">
+              <Form.Field className="form-group">
+                <label htmlFor="username">Customer's Username</label>
+                <select
+                  defaultValue=""
+                  name="customer"
+                  id="customer"
+                  onChange={this.handleFieldChange}
+                  >
+                  <option value="">Select a Customer</option>
+                  {this.props.customers.map(customer => (
+                    <option key={customer.id} id={customer.id} value={customer.url}>
+                      {customer.username}
+                    </option>
+                  ))}
+                </select>
+              </Form.Field>
+                <Form.Field className="form-group">
+                  <label htmlFor="payment_type">Payment Type</label>
+                  <select
+                    defaultValue=""
+                    name="payment_type"
+                    id="payment_type"
+                    onChange={this.handleFieldChange}
+                    >
+                    <option value="">Select a Payment Type</option>
+                    {this.props.paymentTypes.map(payment => (
+                      <option key={payment.id} id={payment.url}>
+                        {payment.account_number}
+                      </option>
+                    ))}
+                  </select>
+                </Form.Field>
+                <Form.Field>
+                <label htmlFor="products">Products</label>
+                <select
+                  defaultValue=""
+                  name="products"
+                  id="products"
+                  onChange={this.props.handleFieldChange}
+                  >
+                  <option value="">Select</option>
+                  {this.props.products.map(product => (
+                    <option key={product.id} id={product.url}>
+                      {product.name}
+                    </option>
+                  ))}
+                </select>
+                </Form.Field>
+
+
+
                     <div className="form-group">
                         <label htmlFor="orderPaymentType">Change Payment Type</label>
                         <input type="email" required className="form-control" onChange={this.handleFieldChange} id="email" value={this.state.email} />
@@ -61,8 +115,10 @@ export default class OrdersEdit extends Component {
                     </div>
                     <Button type="submit" size="tiny" color="green" className="btn btn-primary" onClick={this.editExistingOrder}>Submit Edited Order</Button>
                     <Button as={Link} size="tiny" color="yellow" className="card-link" to={`/ecommerce/orders/`}>Back</Button>
-                </form>
-            </div>
+                {/* </form> */}
+            {/* </div> */}
+            </Form>
+            </React.Fragment>
         );
     }
 }
