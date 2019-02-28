dc8fac64374bc7c12942c0caca7d35c82c0f0ace
robbyhecht <robbyhecht@gmail.com>
2019-02-27 19:24:22 -0600

    small style change

--- src/components/ecommerce/orders/Orders.js
+++ src/components/ecommerce/orders/Orders.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Card } from "semantic-ui-react";
+import { Button, Card, CardDescription } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 export default class Orders extends Component {
@@ -9,17 +9,20 @@ export default class Orders extends Component {
           <Card.Content>
             <div className="card-body">
               <div className="card-title">
-              <Card.Header>Order #{this.props.order.id}</Card.Header>
+              <Card.Header textAlign="center">Order #{this.props.order.id}</Card.Header>
+
               {console.log(this.props.customers)}
               {console.log(this.props.products)}
               {console.log(this.props.paymentTypes)}
               {console.log(this.props.customers.id)}
-
+              
+              <Card.Description>
               <ul style={{listStyleType: "none"}}>
                 <li>Customer: {this.props.customers.username}</li>
                 <li>Products: {this.props.products.name}</li>
                 <li>Payment: {this.props.paymentTypes.payment_name}</li>
               </ul>
+              </Card.Description>
 
               <Button
                 as={Link}
