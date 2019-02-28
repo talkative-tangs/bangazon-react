525cb591ed2bcd1888e177fb3c400143a9efe04d
robbyhecht <robbyhecht@gmail.com>
2019-02-27 18:00:35 -0600

    orders edit is functional

--- src/components/ecommerce/orders/OrdersEdit.js
+++ src/components/ecommerce/orders/OrdersEdit.js
@@ -6,7 +6,7 @@ export default class OrdersEdit extends Component {
     state = {
         customer: "",
         payment_type: "",
-        product: "",
+        products: "",
         id: ""
     }
 
@@ -23,7 +23,7 @@ export default class OrdersEdit extends Component {
         console.log(orders)
         newState.customer = orders.customer
         newState.payment_type = orders.payment_type
-        newState.product = orders.product
+        newState.products = orders.products
         newState.id = orders.id
 
         this.setState(newState)
@@ -34,7 +34,7 @@ export default class OrdersEdit extends Component {
         const orders = {
             customer: this.state.customer,
             payment_type: this.state.payment_type,
-            product: this.state.product,
+            products: this.state.products,
             id: this.state.id
         }
         let ordersURL = "http://localhost:8000/api/v1/orders/"
@@ -45,15 +45,11 @@ export default class OrdersEdit extends Component {
 
     render() {
         return (
-            <div className="container">
-                <form className="editorderForm">
-                    <div className="form-group">
-                        <label htmlFor="orderCustomer">Change Customer</label>
-                        <input type="text" required className="form-control" onChange={this.handleFieldChange} id="customer" value={this.state.customer} />
-                    </div>
 
-            <React.Fragment>
-            <Form className="OrdersEditForm">
+          <React.Fragment>
+
+            <Form className="ordersEditForm">
+
               <Form.Field className="form-group">
                 <label htmlFor="username">Customer's Username</label>
                 <select
@@ -70,55 +66,48 @@ export default class OrdersEdit extends Component {
                   ))}
                 </select>
               </Form.Field>
-                <Form.Field className="form-group">
-                  <label htmlFor="payment_type">Payment Type</label>
-                  <select
-                    defaultValue=""
-                    name="payment_type"
-                    id="payment_type"
-                    onChange={this.handleFieldChange}
-                    >
-                    <option value="">Select a Payment Type</option>
-                    {this.props.paymentTypes.map(payment => (
-                      <option key={payment.id} id={payment.url}>
-                        {payment.account_number}
-                      </option>
-                    ))}
-                  </select>
-                </Form.Field>
-                <Form.Field>
-                <label htmlFor="products">Products</label>
+
+              <Form.Field className="form-group">
+                <label htmlFor="payment_type">Payment Type</label>
                 <select
                   defaultValue=""
-                  name="products"
-                  id="products"
-                  onChange={this.props.handleFieldChange}
+                  name="payment_type"
+                  id="payment_type"
+                  onChange={this.handleFieldChange}
                   >
-                  <option value="">Select</option>
-                  {this.props.products.map(product => (
-                    <option key={product.id} id={product.url}>
-                      {product.name}
+                  <option value="">Select a Payment Type</option>
+                  {this.props.paymentTypes.map(payment => (
+                    <option key={payment.id} id={payment.url}>
+                      {payment.account_number}
                     </option>
                   ))}
                 </select>
-                </Form.Field>
+              </Form.Field>
 
+              <Form.Field>
+              <label htmlFor="products">Products</label>
+              <select multiple
+                defaultValue=""
+                name="products"
+                id="products"
+                onChange={this.props.handleFieldChange}
+                >
+                <option value="">Select</option>
+                {this.props.products.map(product => (
+                  <option key={product.id} id={product.url}>
+                    {product.name}
+                  </option>
+                ))}
+              </select>
+              </Form.Field>
 
+              <Button type="submit" size="tiny" color="green" className="btn btn-primary" onClick={this.editExistingOrder}>Submit Edited Order</Button>
+              <Button as={Link} size="tiny" color="yellow" className="card-link" to={`/ecommerce/orders/`}>Back</Button>
 
-                    <div className="form-group">
-                        <label htmlFor="orderPaymentType">Change Payment Type</label>
-                        <input type="email" required className="form-control" onChange={this.handleFieldChange} id="email" value={this.state.email} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="orderProduct">Change Product</label>
-                        <input type="text" required className="form-control" onChange={this.handleFieldChange} id="product" value={this.state.product} />
-                    </div>
-                    <Button type="submit" size="tiny" color="green" className="btn btn-primary" onClick={this.editExistingOrder}>Submit Edited Order</Button>
-                    <Button as={Link} size="tiny" color="yellow" className="card-link" to={`/ecommerce/orders/`}>Back</Button>
-                {/* </form> */}
-            {/* </div> */}
             </Form>
-            </React.Fragment>
+
+          </React.Fragment>
+
         );
     }
 }
